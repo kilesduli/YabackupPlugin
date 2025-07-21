1e7baf1263b494e5630e32b516e484f0b43b0a06
duli <duli4868@gmail.com>
2025-07-21 20:43:23 +0800

    chore: add detailed header explanation for config options

--- src/main/kotlin/io/github/kilesduli/yabackup/Yabackup.kt
+++ src/main/kotlin/io/github/kilesduli/yabackup/Yabackup.kt
@@ -225,8 +225,35 @@ class Yabackup : JavaPlugin() {
     }
 
     fun setupConfig() {
+        val header = """
+            Yabackup configuration file.
+
+            Below is an explanation of the some options:
+
+            * backup.backups_dir: Path to store backups. Supports both absolute and relative paths.
+            * backup.keep_last_n_backups: Set to 0 to kepp all.
+            * backup.backups_dir_storage_limit: Set to 0 to disable the storage limit.
+            * backup.on_player_join: on join backup.
+            * backup.on_player_quit: on quit backup.
+            * compress.default_type: Supported values: "zstd", "zip".
+            * compress.zstd_level: Valid range: 1-22, Higher means better compression, slower speed.
+            * compress.zip_level: Valid range: 0-9. same above zstd.
+            * interval_backup_task.initial_delay_minutes: Delay after server startup before first interval backup.
+            * interval_backup_task.interval_minutes: Time between each interval backup.
+            * interval_backup_task.skip_if_no_players: interval backups will be skipped when no players are online.
+
+            Note:
+            1. When both keep_last_n_backups and backups_dir_storage_limit are set,
+            the plugin tries to keep the latest N backups within the size limit.
+            If the total size still exceeds the limit, older backups will be deleted anyway.
+            2. When the last player quits, a backup is still created
+            regardless of the value of on_player_quit. This ensures that a backup
+            is still created when the last player leaves, even if it happens between
+            interval backups.
+        """.trimIndent()
+
         config.options().copyDefaults(true)
-        config.options().header("Yabackup Configuration\n")
+        config.options().header(header)
         config.addDefault(Options.BACKUP_BACKUPS_DIR, "./backups")
         config.addDefault(Options.BACKUP_KEEP_LAST_N_BACKUPS, 10)
         config.addDefault(Options.BACKUP_BACKUPS_DIR_STORAGE_LIMIT, 1024) // in MB
